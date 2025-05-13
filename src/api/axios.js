edff641db44c848a18080bd45e72019e9f842abc
josem-1 <161044437+josem-1@users.noreply.github.com>
2025-05-13 10:59:26 -0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //if i got this right, axios does the talking to backend server, i think 
 export default axios.create({
-  baseURL: 'https://mover-frontend-qsnb.onrender.com/api', 
+  baseURL: '/api', //change up variable within quotes for prior to /api to connect to backend hosting
   withCredentials: true, 
 });
