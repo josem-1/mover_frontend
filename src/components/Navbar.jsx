fd88f236293b7efb255e4c0670a2163a022e648c
josem-1 <161044437+josem-1@users.noreply.github.com>
2025-06-11 14:39:27 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React, {useState,useRef, useEffect } from 'react';
 import {Link, useLocation } from 'react-router-dom';
 import {useAuth } from '../contexts/AuthContext';
 import '../styles/Navbar.css';
-//why did i choose this major...............................................................................................
 export default function Navbar() {
   const {user, logout } = useAuth();
   const location = useLocation();//this should make sure the add to list button action only stays in that page'location' 
